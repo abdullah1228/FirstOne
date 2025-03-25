9ef85db2660378666342450c23512cec2ae03d89
abdullah1228 <120764609+abdullah1228@users.noreply.github.com>
2025-03-24 18:04:06 -0700

    Update Lab 01 Task 02 with Task 03.cpp

--- Lab 01 Task 02.cpp
+++ Lab 01 Task 02.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+int linearSearch(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            return i; // Return the index if the key is found
+        }
+    }
+    return -1; // Return -1 if the key is not found
+}
+
 // Function to swap two integers
 void swap(int* a, int* b) {
     int temp = *a;
@@ -39,11 +48,43 @@ void selectionSort(int arr[], int n) {
 int main() {
     // Test with an integer array of size 5
     int intArray[5] = { 64, 25, 12, 22, 11 };
+    int size = sizeof(intArray) / sizeof(intArray[0]);
+    int searchKey = 22;
+
     cout << "Original integer array: ";
-    printArray(intArray, 5);
-    selectionSort(intArray, 5);
+    printArray(intArray, size);
+
+    // Perform linear search
+    int searchResult = linearSearch(intArray, size, searchKey);
+    if (searchResult != -1) {
+        cout << searchKey << " found at index: " << searchResult << endl;
+    }
+    else {
+        cout << searchKey << " not found in the array." << endl;
+    }
+
+    selectionSort(intArray, size);
     cout << "Sorted integer array: ";
-    printArray(intArray, 5);
+    printArray(intArray, size);
+
+    // Perform linear search on the sorted array
+    searchKey = 12;
+    searchResult = linearSearch(intArray, size, searchKey);
+    if (searchResult != -1) {
+        cout << searchKey << " found at index: " << searchResult << endl;
+    }
+    else {
+        cout << searchKey << " not found in the array." << endl;
+    }
+
+    searchKey = 50; // Key not present
+    searchResult = linearSearch(intArray, size, searchKey);
+    if (searchResult != -1) {
+        cout << searchKey << " found at index: " << searchResult << endl;
+    }
+    else {
+        cout << searchKey << " not found in the array." << endl;
+    }
 
     return 0;
 }
